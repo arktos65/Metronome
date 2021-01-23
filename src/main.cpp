e90c8b6cc7c1fcabe9fb64e54935c1be2f5474d7
sean.sullivan <sean@tgwconsulting.co>
2021-01-23 14:13:30 -0700

    Added rudimentary 60 BPM metronome code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
+// Arduino for Musicians
+// Metronome 1
+
 #include <Arduino.h>
 
+// Constants for output of tone to speaker
+const int speakerPin = 9;
+const int frequency = 880;
+const int duration = 50;
+
+// Additional variables
+unsigned int MS_per_beat = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Calculate the amount of delay:
+  // milliseconds per minute / beats per minute
+  unsigned int milliseconds_per_minute = 1000 * 60;
+  unsigned int beats_per_minute = 60;
+  MS_per_beat = milliseconds_per_minute / beats_per_minute;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Output the tone
+  tone(speakerPin, frequency, duration);
+
+  // Delay loop for specified amount of time
+  delay(MS_per_beat);
 }
